471e4f0ca6a1f172778fae663025b8be75fdf5e0
lowrjam <james.lowry.11@gmail.com>
2015-11-22 08:18:33 +1100

    updated intro comment to R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,4 @@
+## Assignment 2 - Lexical Scoping 
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 ## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 ## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve 
